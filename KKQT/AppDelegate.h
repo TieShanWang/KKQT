a6a6124354c157782522624f37c58151da435f71
KK <king@KING-KK.local>
2016-10-07 13:24:33 +0800

    create
    first create

---
+++ KKQT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KKQT
+//
+//  Created by KING on 2016/10/7.
+//  Copyright © 2016年 King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KKQT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KKQT
+//
+//  Created by KING on 2016/10/7.
+//  Copyright © 2016年 King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
